365aa64a541f0d8d5150a4a230fe815689140ff7
manismk <61050821+manismk@users.noreply.github.com>
2022-06-08 20:41:33 +0530

    style: main conatainer and added data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import { ExpandMore, FilterAlt, FilterList, Upload } from "@mui/icons-material";
 import { SecondTopBar } from "./components/secondary-topbar/SecondTopBar";
 import { SideBar } from "./components/sidebar/SideBar";
 import { TopBar } from "./components/topbar/TopBar";
+import { candidateDetails } from "./data";
 
 const App = () => {
   return (
@@ -10,7 +12,44 @@ const App = () => {
       <div className="main">
         <TopBar />
         <SecondTopBar />
-        Helllo
+        <div className="main--container">
+          <div className="top--actions">
+            <div>
+              <div className="action">
+                <span>All Candidates</span>- Active(48)
+                <ExpandMore />
+              </div>
+              <div className="action">
+                <span>Sort By</span>-Last Updated
+                <ExpandMore />
+              </div>
+            </div>
+            <div>
+              <FilterList />
+              <FilterAlt />
+              <Upload />
+            </div>
+          </div>
+          <div className="task--container">
+            {candidateDetails.map((task) => {
+              return (
+                <div className="task--wrapper" key={task.id}>
+                  <p className="task--Name">{task.type}</p>
+                  <div className="candidate--container">
+                    {task.candidates.map((candidate) => {
+                      return (
+                        <div className="candidate--card" key={candidate.id}>
+                          <p className="candidate--name">{candidate.Name}</p>
+                          <p className="candidate--from">{candidate.from}</p>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ src/data.js
@@ -0,0 +1,56 @@
+import { v4 as uuid } from "uuid";
+
+export const candidateDetails = [
+  {
+    id: uuid(),
+    type: "Contacted",
+    candidates: [
+      {
+        id: uuid(),
+        Name: "Mani kandan",
+        from: "Cognizant",
+      },
+      {
+        id: uuid(),
+        Name: "Mani kandan",
+        from: "Cognizant",
+      },
+      {
+        id: uuid(),
+        Name: "Mani kandan",
+        from: "Cognizant",
+      },
+      {
+        id: uuid(),
+        Name: "Mani kandan",
+        from: "Cognizant",
+      },
+    ],
+  },
+  {
+    id: uuid(),
+    type: "Open",
+    candidates: [
+      {
+        id: uuid(),
+        Name: "Mani kandan",
+        from: "Cognizant",
+      },
+      {
+        id: uuid(),
+        Name: "Mani kandan",
+        from: "Cognizant",
+      },
+      {
+        id: uuid(),
+        Name: "Mani kandan",
+        from: "Cognizant",
+      },
+      {
+        id: uuid(),
+        Name: "Mani kandan",
+        from: "Cognizant",
+      },
+    ],
+  },
+];
