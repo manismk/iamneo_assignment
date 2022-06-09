05e70d5187f280af90d64c30fcf2067caa5dd6f4
manismk <61050821+manismk@users.noreply.github.com>
2022-06-09 20:18:10 +0530

    docs: data cleaned up

--- src/data.js
+++ src/data.js
@@ -53,4 +53,72 @@ export const candidateDetails = [
       },
     ],
   },
+  {
+    id: uuid(),
+    type: "Technical 1",
+    candidates: [
+      {
+        id: uuid(),
+        Name: "Mani kandan",
+        from: "Cognizant",
+      },
+      {
+        id: uuid(),
+        Name: "Ajay",
+        from: "Cognizant",
+      },
+      {
+        id: uuid(),
+        Name: "Pranesh",
+        from: "Cognizant",
+      },
+      {
+        id: uuid(),
+        Name: "Barath",
+        from: "Cognizant",
+      },
+    ],
+  },
+  {
+    id: uuid(),
+    type: "Technical 2",
+    candidates: [
+      {
+        id: uuid(),
+        Name: "Krishna",
+        from: "Cognizant",
+      },
+      {
+        id: uuid(),
+        Name: "Keerthana",
+        from: "Cognizant",
+      },
+      {
+        id: uuid(),
+        Name: "Mohan",
+        from: "Cognizant",
+      },
+      {
+        id: uuid(),
+        Name: "Guru",
+        from: "Cognizant",
+      },
+    ],
+  },
+  {
+    id: uuid(),
+    type: "Culture Fit",
+    candidates: [
+      {
+        id: uuid(),
+        Name: "Mani kandan",
+        from: "Cognizant",
+      },
+      {
+        id: uuid(),
+        Name: "Ajay",
+        from: "Cognizant",
+      },
+    ],
+  },
 ];
